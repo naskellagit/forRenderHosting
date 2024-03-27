db9d76d6fa9d6e45fb5390a93aebe786eba83ffd
Максим Кононюк <boryskatowsky@gmail.com>
2024-03-27 17:35:22 +0300

    second commit

--- server/server.js
+++ server/server.js
@@ -1,13 +1,18 @@
 import express from 'express'
+import path from "path"
+import { fileURLToPath } from 'url'
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url))
 
 const PORT = 5000
 
 const app = express()
 
-app.get('/', (req, res) => {
-    return res.send('Здорова други!!!!!! Это халявный хостинг Render!!!')
-})
+app.use('/', express.static(path.join(__dirname, '../', 'client', 'build')))
 
+app.get('/about', (req, res) => {
+    return res.sendFile(path.join(__dirname, '../', 'client', 'build', 'about.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
